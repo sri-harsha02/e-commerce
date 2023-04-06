596c7c0133b71c7bc602bd8216902a732ff73e30
sri-harsha02 <86317644+sri-harsha02@users.noreply.github.com>
2023-04-06 13:44:11 +0530

    Update App.jsx
    commented other imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Cart from "./pages/Cart";
+//import Cart from "./pages/Cart";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Product from "./pages/Product";
-import ProductList from "./pages/ProductList";
-import Register from "./pages/Register";
+//import Login from "./pages/Login";
+//import Product from "./pages/Product";
+//import ProductList from "./pages/ProductList";
+//import Register from "./pages/Register";
 
 const App = () => {
   return <Home/>;
