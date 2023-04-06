9266ce339b0a757b6a751d81033ed4f4ca9241a7
sri-harsha02 <86317644+sri-harsha02@users.noreply.github.com>
2023-04-06 12:51:12 +0530

    first commit
    added public and src folder

---
+++ src/data.js
@@ -0,0 +1,82 @@
+
+ export const sliderItems = [
+    {
+        id:1,
+        img: "https://i.ibb.co/P40K2Fd/w1.png",
+        title : "SUMMER SALE",
+        desc : "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg : "f5fafd",
+    },
+
+    {
+        id:2,
+        img: "https://i.ibb.co/7YDxYxR/w2.png",
+        title : "AUTUMN SALE",
+        desc : "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg : "fcf1ed",
+
+    },
+
+    {
+        id:3,
+        img: "https://i.ibb.co/rmkDRBZ/w3.png",
+        title : "POPULAR  SALE",
+        desc : "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg : "fbf0f4",
+
+    },
+ ]
+
+export const categories = [
+    {
+        id:1,
+        img: "https://images.pexels.com/photos/769749/pexels-photo-769749.jpeg?auto=compress&cs=tinysrgb&w=600",
+        title : "SHIRT STYLE!",
+    },
+    {
+        id:2,
+        img: "https://images.pexels.com/photos/4066288/pexels-photo-4066288.jpeg?auto=compress&cs=tinysrgb&w=600",
+        title : "OVERSIZED TEES",
+    },
+    {
+        id:3,
+        img: "https://images.pexels.com/photos/2229712/pexels-photo-2229712.jpeg?auto=compress&cs=tinysrgb&w=600",
+        title : "DENIM JACKETS",
+    }
+
+]
+
+export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://i.ibb.co/Vg0bYZy/p2.png",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://i.ibb.co/mNJ9p0J/p4.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://i.ibb.co/MMkbMry/p7.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
